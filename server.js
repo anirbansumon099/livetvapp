5496e6fa42ce9a544bc9ddeeb52f151748b81a31
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-11-22 10:05:15 +0600

    Refactor dotenv configuration and update response message

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const playlistRoute = require("./routes/playlist");
 const segmentRoute = require("./routes/segment");
-
-dotenv.config();
 
 const app = express();
 
 app.use("/", playlistRoute);
 app.use("/", segmentRoute);
 
-app.get("/", (req, res) => res.send("HLS Secure Proxy Running..."));
+app.get("/", (req, res) => res.send("HLS Proxy Running"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
