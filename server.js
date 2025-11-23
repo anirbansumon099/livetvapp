a69a2b2b92e4625b126ef7e4e5d758999c5affde
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-11-23 16:25:55 +0600

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/api/data', (req, res) => {
     fs.readFile('./view/data.json', 'utf8', (err, data) => {
         if (err) return res.status(500).json({ error: "File read error" });
         res.json(JSON.parse(data));
-    });
+    });});
 
 
 app.get("/*", (req,res)=>{
