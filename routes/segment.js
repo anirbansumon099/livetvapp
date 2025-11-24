2b1803929a2561fd34e6b6de4d8221748eed0dbf
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-11-24 14:09:02 +0600

    Update segment.js

--- routes/segment.js
+++ routes/segment.js
@@ -5,23 +5,28 @@ const { URL } = require("url");
 
 const router = express.Router();
 
-const baseServer = process.env.MAIN_SERVER + "/live.php";
+const baseServer = process.env.MAIN_SERVER + "/live3.php";
 
 const headerVariants = [
     [
         "Mozilla/5.0 (Android 13; Mobile; rv:145.0) Gecko/145.0 Firefox/145.0",
         "Sec-Fetch-Mode: cors",
-        "Sec-Fetch-Site: same-origin"
+        "Sec-Fetch-Site: same-origin",
+        "Sec-Fetch-Dest:empty"
+
+        
     ],
     [
         "Mozilla/5.0 (Linux; Android 12; SM-A505F) AppleWebKit/537.36 Chrome/118.0 Mobile Safari/537.36",
         "Sec-Fetch-Mode: navigate",
-        "Sec-Fetch-Site: same-origin"
+        "Sec-Fetch-Site: same-origin",
+        "Sec-Fetch-Dest:empty"
     ],
     [
         "Mozilla/5.0 (Windows NT 10.0; Win64) AppleWebKit/537.36 Chrome/120.0 Safari/537.36",
         "Sec-Fetch-Mode: no-cors",
-        "Sec-Fetch-Site: same-origin"
+        "Sec-Fetch-Site: same-origin",
+        "Sec-Fetch-Dest:empty"
     ]
 ];
 
