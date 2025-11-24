57379c4a0bda2179443a7c87a9b7b64ff233fcf0
অনির্বান সুমন <anirbansumon099@gmail.com>
2025-11-24 15:18:28 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ require("dotenv").config();
 const path = require('path');
 const playlistRoute = require("./routes/playlist");
 const segmentRoute = require("./routes/segment");
+const { encryptNumber, decryptNumber } = require("./utils/encryptor");
 
 const app = express();
 //app.use(express.static('.'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // views folder path
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.json())
 // Routes
 app.use("/", playlistRoute);
 app.use("/", segmentRoute);
@@ -34,8 +35,33 @@ app.get("/start",(req,res)=>{
 });
 
 
+app.post("/generate", (req, res) => {
+    const num = parseInt(req.body.number);
 
+    if (isNaN(num)) {
+        return res.status(400).json({ error: "Valid 'number' required" });
+    }
 
+    const encrypted = encryptNumber(num);
+
+    res.json({
+        original: num,
+        encrypted: encrypted
+    });
+});
+
+router.post("/decode", (req, res) => {
+    const code = req.body.code;
+
+    if (!code) return res.status(400).json({ error: "Valid 'code' required" });
+
+    try {
+        const number = decryptNumber(code);
+        res.json({ code: code, number: number });
+    } catch (e) {
+        res.status(400).json({ error: "Invalid encrypted code" });
+    }
+});
 
 app.get("/*", (req,res)=>{
 res.send(`<h1>NOT FOUND </h1> <strong>চেষ্টা করলে অবশ্যই সফল হওয়া যায়।</strong><br>
